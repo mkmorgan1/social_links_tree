ce4ea94b3fe800209f6a5189da79cb65e8a5e0ac
Matthew <m_kmorgan@hotmail.com>
2022-03-13 17:55:04 -0600

    Created the basic structure for the app

---
+++ index.js
@@ -0,0 +1,9 @@
+const express = require('express')
+const app = express()
+const PORT = 80
+
+app.use(express.static('public'))
+
+app.listen(PORT, () => {
+  console.log(`listening on port: ${PORT}`)
+})

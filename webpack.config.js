15ffc922733a17a1de01a60afa85d6a3efc9cea5
Matthew <m_kmorgan@hotmail.com>
2022-04-24 12:32:00 -0600

    FEAT: finalized the webpack

--- webpack.config.js
+++ webpack.config.js
@@ -2,20 +2,22 @@ const path = require('path');
 
 module.exports = {
   entry:'./src/index.js',
-  output: { 
+  output: {
      path: path.resolve(__dirname, 'public'),
      filename: 'bundle.js',
    },
-   rules: [
-    {
-      test: /\.m?js$/,
-      exclude: /(node_modules|bower_components)/,
-      use: {
-        loader: 'babel-loader',
-        options: {
-          presets: ['@babel/preset-env']
+   module: {
+     rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', "@babel/preset-react"]
+          }
         }
       }
-    }
-  ]
-} 1
+    ]
+   }
+}
